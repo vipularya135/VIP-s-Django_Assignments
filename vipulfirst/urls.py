2685e993cab4251d95e7fb6e9458dde1a0023b12
vipul arya <110167247+vipularya135@users.noreply.github.com>
2024-01-18 23:55:56 +0530

    Add files via upload

---
+++ vipulfirst/urls.py
@@ -0,0 +1,23 @@
+# urls.py
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from home.views import home
+from webpage.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('receipes/', receipes, name="receipes"),
+    path('delete/<int:id>/', delete, name="delete_receipe"),
+    path('login/', login_page, name="login_page"),
+    path('logout/', logout_page, name="logout_page"),
+
+    path('register/', register, name="register"),
+
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
